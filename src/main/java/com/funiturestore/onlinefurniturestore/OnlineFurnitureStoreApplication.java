8c95e8551ff1145b203b73053dbc2e05edd6b71a
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-09-27 21:03:05 +0300

    initial commit

---
+++ src/main/java/com/funiturestore/onlinefurniturestore/OnlineFurnitureStoreApplication.java
@@ -0,0 +1,13 @@
+package com.funiturestore.onlinefurniturestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFurnitureStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineFurnitureStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/funiturestore/onlinefurniturestore/OnlineFurnitureStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funiturestore.onlinefurniturestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFurnitureStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
