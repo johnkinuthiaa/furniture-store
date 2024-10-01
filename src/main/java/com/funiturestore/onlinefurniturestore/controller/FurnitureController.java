92a4eef98fe5a533bb694239f8d49557084ab7b2
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-10-01 14:42:24 +0300

    Streams to enhance search activity

--- src/main/java/com/funiturestore/onlinefurniturestore/controller/FurnitureController.java
+++ src/main/java/com/funiturestore/onlinefurniturestore/controller/FurnitureController.java
@@ -2,6 +2,8 @@ package com.funiturestore.onlinefurniturestore.controller;
 
 import com.funiturestore.onlinefurniturestore.model.Furniture;
 import com.funiturestore.onlinefurniturestore.service.FurnitureServiceInterface;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,14 +19,30 @@ public class FurnitureController {
     public List<Furniture> getAllFurniture(){
         return service.getAllFurniture();
     }
+    @GetMapping("/get/by-name")
+    public ResponseEntity<List<Furniture>> getFurnitureByName(@RequestParam String name){
+        return new ResponseEntity<>(service.getFurnitureByName(name), HttpStatus.OK);
+    }
+    @GetMapping("/get/by-condition")
+    public ResponseEntity<List<Furniture>> getFurnitureByCondition(@RequestParam String condition){
+        return new ResponseEntity<>(service.getFurnitureByCondition(condition),HttpStatus.OK);
+    }
+    @GetMapping("/get/by-rating")
+    public ResponseEntity<List<Furniture>> getFurnitureByRating(@RequestParam Float rating){
+        return new ResponseEntity<>(service.getFurnitureByRating(rating),HttpStatus.OK);
+    }
+    @GetMapping("/get/by-rating")
+    public ResponseEntity<List<Furniture>> getFurnitureByPrice(@RequestParam Double price){
+        return new ResponseEntity<>(service.getFurnitureByPrice(price),HttpStatus.OK);
+    }
 
     @PostMapping("/add-furniture")
-    public Furniture createNewFurniture(@RequestBody Furniture furniture,@RequestParam Long id){
-        return service.createNewFurniture(furniture,id);
+    public ResponseEntity<Furniture> createNewFurniture(@RequestBody Furniture furniture,@RequestParam Long id){
+        return new ResponseEntity<>(service.createNewFurniture(furniture,id),HttpStatus.OK);
     }
     @PutMapping("/update-existing-furniture")
-    public Furniture updateExistingFurniture(@RequestBody Furniture furniture,@RequestParam Long id){
-        return service.updateExistingFurniture(furniture,id);
+    public ResponseEntity<Furniture> updateExistingFurniture(@RequestBody Furniture furniture,@RequestParam Long id){
+        return new ResponseEntity<>(service.updateExistingFurniture(furniture,id),HttpStatus.OK) ;
     }
     @DeleteMapping("/Delete-by-id")
     public void deleteFurnitureById(@RequestParam Long id) {
